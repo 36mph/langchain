7d88f56b934209e778bc650ecf82f9d5d1636eb6
Phil Jirsa <phil.jirsa@microsoft.com>
2023-07-14 13:52:45 +0000

    Add cosmosdb loader to init

--- libs/langchain/langchain/document_loaders/__init__.py
+++ libs/langchain/langchain/document_loaders/__init__.py
@@ -37,6 +37,7 @@ from langchain.document_loaders.azlyrics import AZLyricsLoader
 from langchain.document_loaders.azure_blob_storage_container import (
     AzureBlobStorageContainerLoader,
 )
+from langchain.document_loaders.azure_cosmosdb import AzureCosmosDBLoader
 from langchain.document_loaders.azure_blob_storage_file import (
     AzureBlobStorageFileLoader,
 )
@@ -227,6 +228,7 @@ __all__ = [
     "AsyncHtmlLoader",
     "AzureBlobStorageContainerLoader",
     "AzureBlobStorageFileLoader",
+    "AzureCosmosDBLoader",
     "BSHTMLLoader",
     "BibtexLoader",
     "BigQueryLoader",
